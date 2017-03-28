cb1433f353aa534195cbd138f0018d86a4f0741f
Pedro Paul <ppaul804@gmail.com>
2017-03-27 22:33:08 -0300

    Iniciando a implementação do cálculo dos bits de paridade

--- src/Client.java
+++ src/Client.java
@@ -65,7 +65,7 @@ class Client {
             skt.close();
         } catch (Exception e) {
             System.out.print("Ops! Não deu certo!\n");
-            e.printStackTrace();
+            System.out.println(e);
         }
     }
 }
--- src/Hamming.java
+++ src/Hamming.java
@@ -19,7 +19,7 @@ class Hamming {
      * @return Um ArrayList com os "r" dentro
      */
     public static ArrayList<Character> bitRed(char[] msgOri) {
-        int m = msgOri.length;// Quantidade de bits da mensagem
+        int m = msgOri.length;// Quantidade de bits da mensagem original
         int r = 0;// Quantidade de bits de redundância necessários
         boolean bitsSuf = false;// Tem bits sufucientes?
         
@@ -61,7 +61,41 @@ class Hamming {
     }//fim método mostraArrayList
 
     public static ArrayList<Character> calPar(ArrayList<Character> msgRedLis) {
-        throw new UnsupportedOperationException("Calculo dos bits de paridade não desenvolvido ainda...");
-    }
+        
+        //percorre a mensagem com os "r"
+        System.out.println("msgRedLis.size() - 1 -> "+(msgRedLis.size() - 1));
+        for (int i = 0; i < msgRedLis.size() - 1; i++) {
+            System.out.println("i = "+i);
+            //se achar um "r"
+            System.out.println("msgRedLis.get(i) == 'r' -> "+(msgRedLis.get(i) == 'r'));
+            if (msgRedLis.get(i) == 'r') {
+                //percorre a mensagem pulando o resultado da potência de 2 do índice posições começando pelo próprio índice
+                System.out.println("Math.pow(2, i) = "+(Math.pow(2, i)));
+                for (int j = i; i < msgRedLis.size() - 1; j += Math.pow(2, i)) {
+                    System.out.println("j = "+j);
+                    int ct = 0;
+                    System.out.println("ct = "+ct);
+                    //se a posição tiver 1 adiciona ao contador
+                    System.out.println("msgRedLis.get(j) == '1' -> "+(msgRedLis.get(j) == '1'));
+                    if (msgRedLis.get(j) == '1') {
+                        ct++;
+                        System.out.println("ct = "+ct);
+                    }//fim if interno
+                    //se o contador for par
+                    System.out.println("ct % 2 == 0 -> "+(ct % 2 == 0));
+                    if (ct % 2 == 0) {
+                        //no lugar do "r" coloca 1
+                        msgRedLis.set(i, '1');
+                        System.out.println("msgRedLis.set(i, '1') -> "+(msgRedLis.set(i, '1')));
+                    }//fim if da paridade
+                }//fim for interno
+            }//fim if
+        }//fim for
+        
+        mostraArrayList(msgRedLis, "msgRedLis Modificado  = \n");
+        
+        return msgRedLis;
+        
+    }//fim do método calPar
 
 }// fim classe Hamming
